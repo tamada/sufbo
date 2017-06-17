3a64c2a9ffa6d420f0b31bb0a2c29a2cf7f29afa
Haruaki Tamada <tamada@users.noreply.github.com>
2017-06-17 09:21:16 +0900

    fixed bug.
    negative values will be obtained when calling KGram#toInt.
    The return value of KGram#toInt should be positive value.

--- src/main/java/com/github/sufbo/entities/KGram.java
+++ src/main/java/com/github/sufbo/entities/KGram.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 public class KGram {
     private byte[] kgram;
 
-    public KGram(byte[] data, int from, int to){
+    public KGram(byte[] data, int from, int to) {
         kgram = new byte[to - from];
         System.arraycopy(data, from, kgram, 0, kgram.length);
     }
 
-    public int toInt(){
+    public int toInt() {
         int value = 0;
-        for(int i = kgram.length - 1; i >= 0; i--)
-            value = (value << 8) | kgram[i];
+        for (int i = kgram.length - 1; i >= 0; i--)
+            value = (value << 8) | (int) (kgram[i] & 0xff);
         return value;
     }
 
-    public int size(){
+    public int size() {
         return kgram.length;
     }
 
-    public boolean equals(Object object){
+    public boolean equals(Object object) {
         return object instanceof KGram
-                && Arrays.equals(kgram, ((KGram)object).kgram);
+                && Arrays.equals(kgram, ((KGram) object).kgram);
     }
 }
